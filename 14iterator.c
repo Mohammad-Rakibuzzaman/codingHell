d0865832cfab546bc8a4bb47a17fd30fdda09048
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-08-08 07:02:44 +0600

    added till 15

---
+++ 14iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int iterator = 1;
+	
+	printf("%d ", iterator);
+	printf("%d ", iterator += 1);
+	printf("%d ", iterator += 1);
+	printf("%d ", iterator += 1);
+	printf("%d ", iterator += 1);
+	
+	
+	return 0;
+}

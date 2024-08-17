6efdcb40e783e3b360102f1e269ec5d466f797d8
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-08-17 12:09:49 +0600

    added string formatting total

---
+++ python-dsa/02string-formatting.py
@@ -0,0 +1,42 @@
+print("Hello")
+
+print("Hello", "World")
+
+print("Hello", "World", sep="***")
+
+print("Hello", "World", end="***")
+
+print("Hello", end="***")
+print("World")
+
+name = "dip"
+age = 32
+print(name, "is", age, "years old.")
+
+
+print("%s is %d years old." % (name, age))
+
+price = 150
+item = "apple"
+print("The %s costs %10d cents per kg" % (item, price))
+
+print("The %+10s costs %5.3f cents" % (item, price))
+
+print("The %+10s costs %10.2f cents" % (item, price))
+
+item_dict = {"item": "banana", "cost": 24}
+print("The %(item)+10s costs %(cost)7.1f cents" % item_dict)
+
+
+
+
+
+txt1 = "My name is {fname}, I'm {age}".format(fname = "John", age = 36)
+#numbered indexes:
+txt2 = "My name is {0}, I'm {1}".format("John",36)
+#empty placeholders:
+txt3 = "My name is {}, I'm {}".format("John",36)
+
+print(txt1)
+print(txt2)
+print(txt3)

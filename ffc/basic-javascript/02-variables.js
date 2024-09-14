f22808e5100b2c4e82f6b97f76cad7118d593d45
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-09-14 14:18:35 +0600

    added .gitignore and updated ffc folder

---
+++ ffc/basic-javascript/02-variables.js
@@ -0,0 +1,29 @@
+// undefined, null, boolean, string, symbol, bigint, number, and object
+
+// var a = 8
+
+// var myName = "Ratul Bro"
+
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+console.log(a, b, c)
+//output: NaN NaN undefined String!
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+console.log(a, b, c)
---
+++ ffc/basic-javascript/03case-sensitivity.js
@@ -0,0 +1,13 @@
+// Variable declarations
+
+var studlyCapVar;
+var properCamelCase;
+// var titleCaseOver= 2;
+let titleCaseOver = 3;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+
+
+console.log(titleCaseOver)

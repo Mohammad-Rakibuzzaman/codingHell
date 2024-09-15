7287a514162c62c6d0104d5ba6d5b3ca5ab54274
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-09-16 00:21:20 +0600

    folder added from fcc string part done

---
+++ ffc/01-basic-javascript/05strngliteralpractice.js
@@ -0,0 +1,18 @@
+// const badStr = 'Finn responds, "Let\'s go!"';
+
+// console.log(badStr)
+
+
+// const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+// console.log(myStr)
+
+const myStrb = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStrb)
+
+const ex = "5\\3"
+console.log(ex)
+
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+console.log(myStr);
---
+++ ffc/01-basic-javascript/06pracconcat.js
@@ -0,0 +1,49 @@
+// const myStr = "This is the start. " + "This is the end."
+
+// console.log(myStr)
+
+// let myStr = "This is the first sentence. ";
+// myStr += "This is the second sentence."
+// console.log(myStr)
+
+
+//length property
+// Setup
+/*
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+console.log(lastNameLength)
+*/
+
+
+//find first letter
+
+// // Setup
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
+
+// // Only change code below this line
+// firstLetterOfLastName = lastName[0]; // Change this line
+// console.log(firstLetterOfLastName)
+
+
+// const lastName = "Lovelace";
+// const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+// console.log(secondToLastLetterOfLastName)
+
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "There was a cute " + myNoun + ". The name of it was Tommy. It was really very " + myAdjective + " and ofcourse it " + myVerb + " so " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+
+console.log(wordBlanks)

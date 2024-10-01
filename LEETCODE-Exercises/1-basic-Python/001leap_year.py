bc250ba0500650fc90144bc31405f987244855f0
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-10-01 22:05:51 +0600

    feat: updated yesterday

--- LEETCODE-Exercises/1-basic-Python/001leap_year.py
+++ LEETCODE-Exercises/1-basic-Python/001leap_year.py
@@ -1,12 +1,12 @@
-# givenYear = int(input())
+givenYear = int(input())
 
-# if(givenYear > 0):
-#     if(givenYear % 4 == 0 and givenYear % 100 != 0 or givenYear % 400 == 0):
-#         print(givenYear, " is Leap year")
-#     else:
-#         print("Not leap year")
-# else:
-#     print("Invalid year")
+if givenYear > 0:
+    if givenYear % 4 == 0 and (givenYear % 100 != 0 or givenYear % 400 == 0):
+        print(givenYear, " is Leap year")
+    else:
+        print("Not leap year")
+else:
+    print("Invalid year")
 
 
 # year = int(input("enter the year "))
@@ -17,24 +17,22 @@
 #             print(year, "is a leap year")
 #         else:
 #             print(year, "is not a leap year")
-            
+
 #     else:
 #         print(year, "is leap year")
-        
-# else: 
-#     print(year, "is not leap year")
-    
-            
-            
-year = 1900
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f"{year} is a leap year.")
-        else:
-            print(f"{year} is not a leap year.")
-    else:
-        print(f"{year} is a leap year.")
-else:
-    print(f"{year} is not a leap year.")
 
+# else:
+#     print(year, "is not leap year")
+
+
+# year = 1900
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(f"{year} is a leap year.")
+#         else:
+#             print(f"{year} is not a leap year.")
+#     else:
+#         print(f"{year} is a leap year.")
+# else:
+#     print(f"{year} is not a leap year.")

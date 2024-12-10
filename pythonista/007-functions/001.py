a07f6ced76bd0bceaacb9c097bc95b8a873637e1
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-12-10 14:31:34 +0600

    add function 1 problem and other approach

--- pythonista/007-functions/001.py
+++ pythonista/007-functions/001.py
@@ -3,3 +3,14 @@ Write a function called calculate_area that takes base and height as an input an
 area = (1/2)*base*height
 
 '''
+
+def calculate_area(base, height):
+    area = (1/2) * base * height
+    return area
+
+baseOfArea = int(input("Give me the base(Sq feet): "))
+heightOfArea = int(input("Give me the height(Sq feet): "))
+
+solProb = calculate_area(baseOfArea, heightOfArea)
+
+print("So the area of your triangle is: ", solProb)
---
+++ pythonista/007-functions/002.py
@@ -0,0 +1,8 @@
+'''
+Modify above function to take third parameter shape type. It can be either "triangle" or "rectangle". 
+Based on shape type it will calculate area. Equation of rectangle's area is,
+rectangle area=length*width
+
+'''
+
+
